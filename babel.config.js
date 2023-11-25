b736e05cc12f2cc5a1ed846334dc2527793e552e
dkmqflx <dkmqflx@gmail.com>
2023-11-25 14:45:18 +0900

    3 - Hello World

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
 };
+
+/**
+ * babel-preset-expo presets extends the default react native preset
+ * this configuration add supports for decorators
+ * tree shaking web packages
+ * loading font and icons with optional native dependencies provided they're installed
+ */
