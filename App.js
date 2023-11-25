34616a8c86b3f3de6047eb8bc810ebaaa50f2c62
dkmqflx <dkmqflx@gmail.com>
2023-11-25 17:07:08 +0900

    18 - Custom Components

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
 import { View, Alert, Button } from "react-native";
+import Greet from "./components/Greet";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="alert"
-        onPress={() => Alert.alert("Invalid data!")}
-      ></Button>
-
-      <Button
-        title="alert2"
-        onPress={() => Alert.alert("Invalid data!", "DDDD")}
-      ></Button>
-
-      <Button
-        title="alert3"
-        onPress={() =>
-          Alert.alert("Invalid data!", "DDDD", [
-            {
-              text: "Third",
-              onPress: () => console.log("cancel Process"),
-            },
-            {
-              text: "Fourth",
-              onPress: () => console.log("ok Process"),
-            },
-          ])
-        }
-      ></Button>
+      <Greet name="kkim"></Greet>
     </View>
   );
 }
